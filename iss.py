7cad17a1164530c0199f1e9d66c3edf42ec72288
Kevin Blount <kvbchamp@gmail.com>
2020-07-03 15:37:32 -0400

    add more notes dev plan notes - API links, etc

--- iss.py
+++ iss.py
@@ -2,14 +2,27 @@
 
 """
 Part A
-List of astronauts - full names, spacecraft, total # of astronauts in 
+List of astronauts - full names, spacecraft, total # of astronauts in
+Use this API - http://api.open-notify.org/astros.json
 
 Part B
 Obtain current geographic coordinates(lat/lon) of space station and 
-time stamp
+time stamp.
+Use this API - http://api.open-notify.org/iss-now.json
 
 Part C
+With turtle library, create graphics screen with the world map
+background image, map.gif.
+Use turtle methods such Screen(), setup(), bgpic(), and setworldcoordinates()
+Register an icon image for the ISS within the turtle screen context.
+Create a turtle.Turtle() to move the ISS ot its current lat/lon on the map -
+use methods such as shape(), setheading(), penup(). amd goto()
+Python.org Turtle Documentation - https://docs.python.org/3/library/turtle.html
 
+Part D
+Find out the next time that the ISS will be overhead of Indianapolis, Indiana
+Use geographic lat/lon of Indianapolis, Indiana to plot a yellow dot on the map.
+Use this API to query the next pass - http://api.open-notify.org/iss-pass.json
 """
 
 __author__ = 'Kevin Blount'
